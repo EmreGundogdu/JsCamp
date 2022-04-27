3b2735c6087e8a4de5fe7d35e6890890a5c3e2d1
Emre GÜNDOĞDU <emre26gundogdu@gmail.com>
2022-04-27 14:22:53 +0300

    added some fucntions

---
+++ src/functions.js
@@ -0,0 +1,12 @@
+function addToCart(quantity,productName="Elma") {
+    console.log("Adet: "+ quantity + " eklenen: "+ productName)
+}
+addToCart(10)
+
+let hello = () => {
+    console.log("Merhaba")
+}
+hello()
+let hello2 = () => {
+    console.log("Merhaba hello2")
+}

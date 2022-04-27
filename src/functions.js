329cdd6fa564385216c7d0095e647e3e465d7eb0
Emre GÜNDOĞDU <emre26gundogdu@gmail.com>
2022-04-27 17:33:54 +0300

    new example

--- src/functions.js
+++ src/functions.js
@@ -37,3 +37,20 @@ let urunler = [
     { productName: "Software", price: 2500, quantity: 20 }
 ]
 addToCart4(urunler)
+
+// function topla(sayi1, sayi2) {
+//      let sonuc = sayi1 + sayi2
+//      console.log(sonuc)
+//     console.log(sayi1 + sayi2)
+// }
+// topla(3, 5)
+
+function add(something, ...numbers) { //rest operatörü
+    let toplam = 0
+    for (let index = 0; index < numbers.length; index++) {
+        toplam += numbers[index]
+    }
+    console.log(something)
+    console.log(toplam)
+}
+add(123, 30, 40)

b03210cb3d9e66a71ae522b4283479ec0357f7fd
Emre GÜNDOĞDU <emre26gundogdu@gmail.com>
2022-04-27 15:09:07 +0300

    added examples about functions

--- src/functions.js
+++ src/functions.js
@@ -1,5 +1,5 @@
-function addToCart(quantity,productName="Elma") {
-    console.log("Adet: "+ quantity + " eklenen: "+ productName)
+function addToCart(quantity, productName = "Elma") {
+    console.log("Adet: " + quantity + " eklenen: " + productName)
 }
 addToCart(10)
 
@@ -10,3 +10,30 @@ hello()
 let hello2 = () => {
     console.log("Merhaba hello2")
 }
+
+function addToCart2(productName, quantity, unitPrice) {
+
+}
+addToCart2("Bmw", 1, 50000)
+
+
+let product1 = {
+    productName: "Bmw",
+    unitPrice: 50000,
+    quantity: 1
+}
+function addToCart3(product) {
+    console.log("Adı: " + product1.productName + " Fiyat: " + product1.unitPrice + " Adet: " + product1.quantity)
+}
+addToCart3(product1)
+
+
+function addToCart4(x) {
+    console.log(x)
+}
+let urunler = [
+    { productName: "PC", price: 1500, quantity: 20 },
+    { productName: "Telefon", price: 3500, quantity: 20 },
+    { productName: "Software", price: 2500, quantity: 20 }
+]
+addToCart4(urunler)
